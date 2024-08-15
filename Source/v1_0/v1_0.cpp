48bcb0ee46f20812ae839badb5c1a701034e8608
Dhairya Patel <ts554408@gmail.com>
2024-08-15 11:16:14 +0530

    Firebase Storage Demo
    Added a temporary "Download Assets" button to test the Firebase Storage functionality.

---
+++ Source/v1_0/v1_0.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "v1_0.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, v1_0, "v1_0" );
